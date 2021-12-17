440257d6fe3e9c00b28b3035d0f42bc0a5e2e35f
Alexey Konovalov <53217691+KonovalovAlexey@users.noreply.github.com>
2021-12-17 11:54:15 +0200

    Update main.go

--- cmd/yaml-patch/main.go
+++ cmd/yaml-patch/main.go
@@ -7,7 +7,7 @@ import (
 	"os"
 
 	flags "github.com/jessevdk/go-flags"
-	yamlpatch "github.com/krishicks/yaml-patch"
+	yamlpatch "github.com/ACCELERATOR-SANDBOX/yaml-patch"
 )
 
 type opts struct {
